bbea40bf19ca8a96cf2b269f72f30a7ca874fedd
yunminsu <msg3265@nave.com>
2023-12-08 17:12:28 +0900

    API수정 업로드

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const { client } = require('../database')
+
+const router = express.Router();
+const db = client.db('base');
+
+router.get('/', (req, res) => {
+  res.render('main');
+})
+
+// 프론트에서 값 전달 받아 데이터베이스에 저장
+// router.post('/test', async (req, res) => {
+//   try {
+//     await db.collection('test').insertOne({ 
+//       search: req.body.value 
+//     });
+//     res.send('데이터 저장 완료');
+//   } catch (err) {
+//     console.error(err);
+//   }
+// });
+
+router.get('/list', async (req, res) => {
+  try {
+    const lists = await db.collection('list').find({}).toArray();
+    res.send(lists);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+module.exports = router;
+
+// try {
+//   const res = await axios.post('http://localhost:8088/test', { value })
+//   console.log(res.data);
+// } catch (err) {
+//   console.error(err);
+// }
