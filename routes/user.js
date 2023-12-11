773eb0cb2061a2a43bbc47ec91fc8d63da3674b8
haeun82 <cjwd829@gmail.com>
2023-12-11 14:38:35 +0900

    hash 추가

--- routes/user.js
+++ routes/user.js
@@ -1,27 +1,42 @@
 const express = require('express');
 const passport = require('passport');
+const bcrypt = require('bcrypt');
+
 
 const { client } = require('../database');
 const db = client.db('base');
 
 const router = express.Router();
 
-router.get('/register', async (req, res) => {
-    res.send('dd');
+router.get('/register', (req, res) => {
+  res.send('dd');
 });
 
 router.post('/register', async (req, res) => {
   try {
     const { username, password, pw, email } = req.body;
-    console.log(username, password, pw, email);
+    const existUser = await db.collection('user').findOne({ username });
+    if (existUser) {
+      throw new Error('존재하는 사용자');
+    }
+
+    const hash = await bcrypt.hash(password, 12);
     await db.collection('user').insertOne({
       username,
-      password,
-      pw,
+      password : hash,
+      pw: hash,
       email
+    });
+    res.json({
+      flag: true,
+      message: '회원 가입 성공'
     });
   } catch (err) {
     console.error(err);
+    res.json({
+      flag: false,
+      message: err.message
+    });
   }
 });
 
