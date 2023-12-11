511675a63168c8e90458e23f5650738a2e895bd8
fkdkskdfjfk <yljw225@naver.com>
2023-12-11 16:32:34 +0900

    login

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,19 @@ const db = client.db('base');
 
 router.get('/', (req, res) => {
   res.render('main');
-})
+});
+
+router.post('/', (req, res) => {
+  if (req.user) {
+    res.json({
+      flag: true,
+    });
+  } else {
+    res.json({
+      flag: false
+    })
+  }
+});
 
 // 프론트에서 값 전달 받아 데이터베이스에 저장
 // router.post('/test', async (req, res) => {
--- routes/user.js
+++ routes/user.js
@@ -9,8 +9,18 @@ const router = express.Router();
 
 // 로그인 로그아웃 라우터
 router.get('/login', isNotLoggedIn, (req, res) => {
+  // console.log(req.user);
   // res.render('login');
   // res.send('dsf');
+  if (req.user) {
+    res.json({
+      flag: true,
+    });
+  } else {
+    res.json({
+      flag: false
+    })
+  }
 });
 
 // POST /user/login
@@ -18,9 +28,6 @@ router.post('/login', isNotLoggedIn, inputCheck, (req, res, next) => {
   passport.authenticate('local', (authError, user, info) => {
     // user: 성공 시 로그인한 사용자 정보
     // info: 실패 시 이유
-    console.log(authError);
-    console.log(user);
-    console.log(info);
     if (authError) {
       console.error(authError);
       return res.status(500).json(authError);
@@ -31,12 +38,15 @@ router.post('/login', isNotLoggedIn, inputCheck, (req, res, next) => {
     // passport.serializeUser가 호출됨
     // user 객체가 serializeUser로 넘어가게 됨(index.js)
     req.login(user, (loginError) => {
-      console.log(user.username);
       if (loginError) {
         console.log(loginError);
         return next(loginError);
       }
-      res.redirect('/');  // 로그인 완료 시 실행할 코드, 동기식으로 보냈기 때문에 redirect, 비동기면 res.json보냄
+      res.json({
+        flag: true,
+        message: 'login success'
+      });
+      // res.redirect('/');  // 로그인 완료 시 실행할 코드, 동기식으로 보냈기 때문에 redirect, 비동기면 res.json보냄
     });
   })(req, res, next);
 });
@@ -44,7 +54,7 @@ router.post('/login', isNotLoggedIn, inputCheck, (req, res, next) => {
 
 // GET /user/logout
 // 우발적, 악의적 로그아웃을 방지하려면 GET 요청 대신 POST 또는 DELETE 요청 사용하면 좋음
-router.get('/logout', isLoggedIn, (req, res, next) => {
+router.post('/logout', isLoggedIn, (req, res, next) => {
   // logout: req.user 객체와 req.session 객체를 제거
   req.logout((logoutError) => {  // 두 객체 제거 후 콜백 함수가 실행됨
     if (logoutError) return next(logoutError);
