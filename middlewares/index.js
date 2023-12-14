29127d19c70870ec317527029383122e3de0317f
fkdkskdfjfk <yljw225@naver.com>
2023-12-14 12:28:09 +0900

    car

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,7 +2,9 @@ const isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) { 
     next();
   } else {
-    res.status(401).send('로그인 필요');
+    // res.status(401).send('로그인 필요');
+    const message = encodeURIComponent('로그인 필요');
+    res.redirect(`/error=${message}`);
   }
   };
 
--- routes/user.js
+++ routes/user.js
@@ -39,9 +39,6 @@ router.post('/register', async (req, res) => {
 });
 // 로그인 로그아웃 라우터
 router.get('/login', isNotLoggedIn, (req, res) => {
-  // console.log(req.user);
-  // res.render('login');
-  // res.send('dsf');
   if (req.user) {
     res.json({
       flag: true,
@@ -56,17 +53,12 @@ router.get('/login', isNotLoggedIn, (req, res) => {
 // POST /user/login
 router.post('/login', isNotLoggedIn, inputCheck, (req, res, next) => {
   passport.authenticate('local', (authError, user, info) => {
-    // user: 성공 시 로그인한 사용자 정보
-    // info: 실패 시 이유
     if (authError) {
       console.error(authError);
       return res.status(500).json(authError);
     }
     if (!user) return res.status(401).json(info.message);
 
-    // login(): 사용자 정보를 세션에 저장하는 작업을 시작
-    // passport.serializeUser가 호출됨
-    // user 객체가 serializeUser로 넘어가게 됨(index.js)
     req.login(user, (loginError) => {
       if (loginError) {
         console.log(loginError);
@@ -75,8 +67,6 @@ router.post('/login', isNotLoggedIn, inputCheck, (req, res, next) => {
       res.json({
         flag: true,
         message: 'login success',
-        // id: req.user._id,
-        // username: req.user.username
         user: req.user,
       });
     });
@@ -94,14 +84,13 @@ router.post('/loginCheck', async (req, res, next) => {
   }
 });
 
+
 // GET /user/logout
-// 우발적, 악의적 로그아웃을 방지하려면 GET 요청 대신 POST 또는 DELETE 요청 사용하면 좋음
-router.post('/logout', (req, res, next) => {
-  // logout: req.user 객체와 req.session 객체를 제거
-  req.logout((logoutError) => {  // 두 객체 제거 후 콜백 함수가 실행됨
+router.post('/logout', isLoggedIn, (req, res, next) => {
+  req.logout((logoutError) => {
     if (logoutError) return next(logoutError);
     res.clearCookie('connect.sid');
-    res.redirect('/');  // 로그아웃 완료 시 실행할 코드
+    res.redirect('/');  
   });
 });
 
