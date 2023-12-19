a3d483003c1d119166e4573f626bf73e1005ffa2
yunminsu <msg3265@nave.com>
2023-12-19 15:16:41 +0900

    배포 서버 실행

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.set('view engine', 'ejs');
 connect();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://event-moum.netlify.app/' ],
   methods: ['GET', 'POST', 'OPTIONS'],
   credentials: true
 }));
